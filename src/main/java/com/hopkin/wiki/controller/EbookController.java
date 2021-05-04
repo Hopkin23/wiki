4b858e143e025de092bfd89de6dc14ca6c5d2ec0
Hopkin <1062562441@qq.com>
2021-05-04 14:49:23 +0800

    1-14 完成电子书列表查询接口

---
+++ src/main/java/com/hopkin/wiki/controller/EbookController.java
@@ -0,0 +1,35 @@
+package com.hopkin.wiki.controller;
+
+import com.hopkin.wiki.domain.Ebook;
+import com.hopkin.wiki.service.EbookService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    //注入自定义配置项
+    @Value("${ebook.hello:TEST}")
+    private String ebookHello;
+
+    @Resource
+    private EbookService ebookService;
+
+    @PostMapping("/post")
+    public String helloPost(String name){
+        return "Hello World! Post."+name;
+    }
+
+    @GetMapping("/list")
+    public List<Ebook> list(){
+        System.out.println("list");
+        return ebookService.list();
+    }
+}
