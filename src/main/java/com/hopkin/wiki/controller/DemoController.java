38deb41deb4e841aaee4bdbd155ae02ace0055dd
Hopkin <1062562441@qq.com>
2021-05-04 14:38:16 +0800

    1-12 增加demo表列表查询接口，并测试

--- src/main/java/com/hopkin/wiki/controller/DemoController.java
+++ src/main/java/com/hopkin/wiki/controller/DemoController.java
@@ -1,33 +1,35 @@
 package com.hopkin.wiki.controller;
 
-import com.hopkin.wiki.domain.Test;
-import com.hopkin.wiki.service.TestService;
+import com.hopkin.wiki.domain.Demo;
+import com.hopkin.wiki.service.DemoService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-public class TestController {
+@RequestMapping("/demo")
+public class DemoController {
 
     //注入自定义配置项
-    @Value("${test.hello:TEST}")
-    private String testHello;
+    @Value("${demo.hello:TEST}")
+    private String demoHello;
 
     @Resource
-    private TestService testService;
+    private DemoService demoService;
 
-    @PostMapping("/test/post")
+    @PostMapping("/post")
     public String helloPost(String name){
         return "Hello World! Post."+name;
     }
 
-    @GetMapping("/test/list")
-    public List<Test> list(){
+    @GetMapping("/list")
+    public List<Demo> list(){
         System.out.println("list");
-        return testService.list();
+        return demoService.list();
     }
 }
