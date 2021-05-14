54a8ae94d42581d096c8b7f698c55892b6fc85b5
Hopkin <1062562441@qq.com>
2021-05-14 20:27:16 +0800

    2-10 使用reactive实现数据绑定

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -46,14 +46,15 @@
         </a-layout-sider>
         <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
             <pre>
-               {{ebooks}}
+                {{ebooks}}
+                {{ebooks2}}
             </pre>
         </a-layout-content>
     </a-layout>
 </template>
 
 <script lang="ts">
-    import {defineComponent, onMounted, ref} from 'vue';
+    import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
     import axios from "axios";
 
 
@@ -62,18 +63,21 @@
         setup(){
             console.log("setup");
             const ebooks = ref();
+            const ebooks1 = reactive({books: []});
 
             onMounted(()=>{
                 console.log("onMounted");
                 axios.get("http://localhost:8080/ebook/list?name=Spring").then((response) => {
                     const data = response.data;
                     ebooks.value = data.content;
+                    ebooks1.books = data.content;
                     console.log(response);
                 })
             })
 
             return {
-                ebooks
+                ebooks,
+                ebooks2: toRef(ebooks1, "books")
             }
         }
     });
